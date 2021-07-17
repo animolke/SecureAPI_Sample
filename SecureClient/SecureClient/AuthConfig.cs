60fa37ba61950ab35d6e6ab9fa8bfc9603115521
ANIKET MOLKE <animolke@gmail.com>
2021-07-17 22:12:21 +0530

    Initial commit - added projects

---
+++ SecureClient/SecureClient/AuthConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using Microsoft.Extensions.Configuration;
+
+namespace SecureClient
+{
+    public class AuthConfig
+    {
+        public string Instance { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string Authority
+        {
+            get {
+                return string.Format(CultureInfo.InvariantCulture, Instance, TenantId);
+            }
+        }
+
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string ResourceId { get; set; }
+
+        public static AuthConfig ReadJsonFromFile(string path) 
+        {
+            IConfiguration configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path);
+
+            configuration = builder.Build();
+            return configuration.Get<AuthConfig>();
+        }
+    }
+}
---
+++ SecureClient/SecureClient/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace SecureClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making the call");
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsync() 
+        {
+            AuthConfig config = AuthConfig.ReadJsonFromFile("appsettings.json");
+            IConfidentialClientApplication app;
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+            string[] ResourceIds = new string[] { config.ResourceId };
+            AuthenticationResult result = null;
+
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token Acquired \n  {0}",result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            if (!string.IsNullOrEmpty(result.AccessToken)) 
+            {
+                var httpClient = new HttpClient();
+                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+
+                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m=> m.MediaType == "application/json")) 
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+                }
+
+                defaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                HttpResponseMessage httpResponse = await httpClient.GetAsync(config.BaseAddress);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string json = await httpResponse.Content.ReadAsStringAsync();
+                    Console.WriteLine($"\n {json}");
+                }
+                else 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Failed API call: {httpResponse.StatusCode}");
+                    string content = await httpResponse.Content.ReadAsStringAsync();
+                    Console.WriteLine(content);
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
